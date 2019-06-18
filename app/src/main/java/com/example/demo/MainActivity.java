ff72d0caf2302cb17091490a898dc09d9866b004
Veronica <veronicatemesgen@yahoo.com>
2019-06-18 15:40:32 -0400

    I have changed it into a timer

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // initializing the features we have on the page
+    private TextView countdownText;
+    private Button countdownButton;
+
+    // setting the timer, giving it a starting point
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 600000; // 600000 = 10 mins
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
